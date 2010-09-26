3da88cd61d2992d54df8fa3623e8041c9436e916
JamesFerguson <james@getup.org.au>
2010-09-26 16:40:47 +1000

    First commit for JemimiaFerguson.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jemimaferguson::Application.config.secret_token = 'fff4808c5835762608e2c091b6c0abee15005aa429cc833783affad788e0cafd29cedd1616ce8d82425e34c35a97afacdfe9cb23eaf4d63bad3cc94d231d6083'
